4ca8d78b543b2ec0be120da59b6316d8cd431873
yoannlatzer <yoannlatzer@gmail.com>
2016-06-01 10:33:20 +0200

    (broken) Scatterplot
    Data is wrongly loaded (the points don't catch the right values).
    There are also refreshing problems with the axis

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -1,7 +1,7 @@
 /*
- * flowChart.js
+ * scatterplot.js
  * Part of the Team Visualization.
- * Shows the flow between exporting and importing countries.
+ * Shows 
  *
  * Authour: Wybe Westra
  * Date: 30-05-2016
@@ -19,34 +19,34 @@ function createScatterplot(container, config) {
     size.height = size.totalHeight - size.margin.top - size.margin.bottom;
 
     // The return value.
-    var flowchart = {
-        class: "flowchart",
+    var scatterplot = {
+        class: "scatterplot",
         datapointClass: "datapoint",
     };
 
-    flowchart.datapointSelector =
-        "." + flowchart.class + " ." + flowchart.datapointClass;
+    scatterplot.datapointSelector =
+        "." + scatterplot.class + " ." + scatterplot.datapointClass;
 
     // Scales.
     var importY = d3.scale.linear()
         .range([0, size.height]);
 
-    var exportY = d3.scale.linear()
-        .range([0, size.height]);
+    var exportX = d3.scale.linear()
+        .range([0, size.width]);
 
 
     // ---- Build the chart ----------------------------------------------------
 
     var chart = d3.select(container).append("svg")
-        .classed(flowchart.class, true)
+        .classed(scatterplot.class, true)
         .attr("width", size.totalWidth)
         .attr("height", size.totalHeight)
-      .append("g") // Add the margin offset.
+        .append("g") // Add the margin offset.
         .attr("transform", "translate(" + size.margin.left + "," + size.margin.top + ")");
 
     // ---- Update function ----------------------------------------------------
 
-    flowchart.update = function(config) {
+    scatterplot.update = function(config) {
         // Get the currenly selected data.
         var data = config.nestedData
             .get(config.year)
@@ -66,55 +66,67 @@ function createScatterplot(container, config) {
         );
 
         importY.domain([0, maxScaleSize]);
-        exportY.domain([0, maxScaleSize]);
+        exportX.domain([0, maxScaleSize]);
+
+        // Setup X and Y Axis
+        var yAxis = d3.svg.axis().scale(importY).orient("left").ticks(10);
+        var xAxis = d3.svg.axis().scale(exportX).orient("bottom").ticks(5);
+
+        // Draw X Axis
+        chart.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + size.height + ")")
+            .call(xAxis)
+            .style("fill", "green")
+        .append("text")
+            .attr("class", "label")
+            .attr("x", size.width)
+            .attr("y", -6)
+            .style("text-anchor", "end")
+            .text("Export");
+
+        // Draw Y Axis
+        chart.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .style("fill", "green")
+        .append("text")
+            .attr("class", "label")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Import");
 
         // ---- Datapoints -----------------------------------------------------
-
-        var importDatapoint = chart.selectAll("g.import")
+        var datapoints = chart.selectAll("g")
             .data(importData);
 
-        var exportDatapoint = chart.selectAll("g.export")
-            .data(exportData);
-
         // -- Enter --
-
-        var newImportDatapoint = importDatapoint.enter().append("g")
-            .classed(flowchart.datapointClass, true)
-            .classed("import", true);
-
-        var newExportDatapoint = exportDatapoint.enter().append("g")
-            .classed(flowchart.datapointClass, true)
-            .classed("export", true);
-
-        newImportDatapoint.append("rect");
-        newExportDatapoint.append("rect");
+        var newDatapoints = datapoints.enter().append("g")
+            .classed(scatterplot.datapointClass, true)
+            .classed("dots", true);
 
         // -- Update --
 
-        importDatapoint.select("rect")
-            .attr("x", 0)
-            .attr("y", function(d) { return importY(d.y0); })
-            .attr("width", 50)
-            .attr("height", function(d) { return importY(d.y1 - d.y0); });
-
-        exportDatapoint.select("rect")
-            .attr("x", 60)
-            .attr("y", function(d) { return exportY(d.y0); })
-            .attr("width", 50)
-            .attr("height", function(d) { return exportY(d.y1 - d.y0); });
+        chart.selectAll(".dots")
+          .append("circle")
+            .attr("class", "dot")
+            .attr("r", 5)
+            .attr("cx", function(d) { return exportX(d.y0); })
+            .attr("cy", function(d) { return importY(d.y1); })
+            .style("fill", "red");
 
         // -- Remove --
 
-        importDatapoint.exit()
+        datapoints.exit()
             .remove();
 
-        exportDatapoint.exit()
-            .remove();
     }
 
     // ---- Return the values --------------------------------------------------
 
-    return flowchart;
+    return scatterplot;
 }
 
 
